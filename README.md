25340af886cbc9598fddf1f91e8cbdb16427ca71
Euan Torano <euantorano@gmail.com>
2018-10-18 15:36:28 +0100

    Simplify install as recommended by reviewer
    Signed-off-by: Euan Torano <euantorano@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Zig Chocolatey Package
 
 This repository contains the content of a [Chocolatey](https://chocolatey.org) package to install the [Zig](https://ziglang.org/) programming language.
+
+## Building and pushing the package
+
+- **Build**: `choco pack`
+- **Test install**: `choco install zig -dv -s .`
+- **Publish**: `choco push zig.[VERSION].nupkg -s https://push.chocolatey.org/`
--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -7,23 +7,10 @@ $packageDownloadUrl = "https://ziglang.org/download/$packageVersion/$packageZipF
 $packageChecksum = 'bb568c03950958f8bb3472139c3ab5ed74547c8c694ab50f404c202faf51baf4'
 $packageChecksumType = 'sha256'
 
-Get-ToolsLocation
-$tools = $env:ChocolateyToolsLocation
-
-$zigRoot = Join-Path "$tools" 'zig'
-
-Write-Host "Installing to: $zigRoot"
-
-if (Test-Path "$zigRoot") {
-    Remove-Item "$zigRoot" -Recurse
-}
+$zigRoot = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
 Install-ChocolateyZipPackage -PackageName "$packageName" `
     -Url64bit "$packageDownloadUrl" `
     -Checksum64 "$packageChecksum" `
     -ChecksumType64 "$packageChecksumType" `
-    -UnzipLocation "$tools"
-
-Rename-Item -Path (Join-Path "$tools" "$packageZipFileName") -NewName "$zigRoot"
-
-Install-ChocolateyPath -PathToInstall "$zigRoot" -PathType 'Machine'
+    -UnzipLocation "$zigRoot"
