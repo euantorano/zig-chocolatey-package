25fb7681d182b9c4e0d6eafdadd919d2daa17797
Euan Torano <euan@torano.co.uk>
2021-06-11 14:45:10 +0100

    Update to Zig 0.8.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'zig'
-$packageVersion = '0.7.1'
+$packageVersion = '0.8.0'
 $packageZipFileName = "zig-windows-x86_64-$packageVersion"
 $packageDownloadUrl = "https://ziglang.org/download/$packageVersion/$packageZipFileName.zip"
-$packageChecksum = '4818a8a65b4672bc52c0ae7f14d014e0eb8caf10f12c0745176820384cea296a'
+$packageChecksum = '8580fbbf3afb72e9b495c7f8aeac752a03475ae0bbcf5d787f3775c7e1f4f807'
 $packageZipFileName32Bit = "zig-windows-i386-$packageVersion"
 $packageDownloadUrl32Bit = "https://ziglang.org/download/$packageVersion/$packageZipFileName32Bit.zip"
-$packageChecksum32Bit = 'a1b9a7421e13153e07fd2e2c93ff29aad64d83105b8fcdafa633dbe689caf1c0'
+$packageChecksum32Bit = 'b6ec9aa6cd6f3872fcb30d43ff411802d82008a0c4142ee49e208a09b2c1c5fe'
 $packageChecksumType = 'sha256'
 
 $zigRoot = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
